229601cc2c3c125cf004961fc6f8bea260efdf41
AG6789 <99586233+AG6789@users.noreply.github.com>
2022-02-13 14:23:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 12;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
